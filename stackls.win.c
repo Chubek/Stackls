5def5447c952a3217662095af260267a2d1ef2c9
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-23 14:00:52 +0330

    fixed errors reported

--- stackls.win.c
+++ stackls.win.c
@@ -1,21 +1,11 @@
 #define WIN32_LEAN_AND_MEAN
-#include <dbghep.h>
+#include <dbghelp.h>
 #include <errhandlingapi.h>
-#include <fileapi.h>
-#include <handleapi.h>
-#include <limits.h>
-#include <process.h>
 #include <shellapi.h>
 #include <strsafe.h>
-#include <tchar.h>
-#include <tlhep32.h>
+#include <tlhelp32.h>
+#include <winbase.h>
 #include <windows.h>
-
-#if !defined(_WIN32) || !defined(_WIN64) || !defined(__windows__) || !defined(__WINDOWS__)
-#warning "Compliant predefined CPP macros not detected."
-#warning "This code is designed to be compiled and ran under the Microsoft Windows operating system."
-#warning "A GNU Linux version is provided."
-#endif
 
 #ifdef __GNUC__
 #define _normal_inline static inline __attribute__((always_inline))
@@ -41,11 +31,11 @@
 #define ASSIGN_MACHINE_VALUES(STACKW, CONTEXT)                                                                         \
     do                                                                                                                 \
     {                                                                                                                  \
-        STACKW->AddrPC.Offset = CONTEXT.Eip;                                                                           \
+        STACKW->AddrPC.Offset = CONTEXT->Eip;                                                                          \
         STACKW->AddrPC.Mode = AddrModeFlat;                                                                            \
-        STACKW->AddrFrame.Offset = CONTEXT.Ebp;                                                                        \
+        STACKW->AddrFrame.Offset = CONTEXT->Ebp;                                                                       \
         STACKW->AddrFrame.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrStack.Offset = CONTEXT.Esp;                                                                        \
+        STACKW->AddrStack.Offset = CONTEXT->Esp;                                                                       \
         STACKW->AddrStack.Mode = AddrModeFlat;                                                                         \
     } while (0)
 #elif defined(_M_IX64) || defined(_M_AMD64) || defined(__x86_64__) || defined(__amd64__)
@@ -53,11 +43,11 @@
 #define ASSIGN_MACHINE_VALUES(STACKW, CONTEXT)                                                                         \
     do                                                                                                                 \
     {                                                                                                                  \
-        STACKW->AddrPC.Offset = CONTEXT.Rip;                                                                           \
+        STACKW->AddrPC.Offset = CONTEXT->Rip;                                                                          \
         STACKW->AddrPC.Mode = AddrModeFlat;                                                                            \
-        STACKW->AddrFrame.Offset = CONTEXT.Rbp;                                                                        \
+        STACKW->AddrFrame.Offset = CONTEXT->Rbp;                                                                       \
         STACKW->AddrFrame.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrStack.Offset = CONTEXT.Rsp;                                                                        \
+        STACKW->AddrStack.Offset = CONTEXT->Rsp;                                                                       \
         STACKW->AddrStack.Mode = AddrModeFlat;                                                                         \
     } while (0)
 #elif defined(_M_IA64) || defined(__ia64__)
@@ -65,13 +55,13 @@
 #define ASSIGN_MACHINE_VALUES(STACKW, CONTEXT)                                                                         \
     do                                                                                                                 \
     {                                                                                                                  \
-        STACKW->AddrPC.Offset = CONTEXT.StIIP;                                                                         \
+        STACKW->AddrPC.Offset = CONTEXT->StIIP;                                                                        \
         STACKW->AddrPC.Mode = AddrModeFlat;                                                                            \
-        STACKW->AddrFrame.Offset = CONTEXT.IntSp;                                                                      \
+        STACKW->AddrFrame.Offset = CONTEXT->IntSp;                                                                     \
         STACKW->AddrFrame.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrStack.Offset = CONTEXT.IntSP;                                                                      \
+        STACKW->AddrStack.Offset = CONTEXT->IntSP;                                                                     \
         STACKW->AddrStack.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrBStore.Offset = CONTEXT.RsBSP;                                                                     \
+        STACKW->AddrBStore.Offset = CONTEXT->RsBSP;                                                                    \
         STACKW->AddrBStore.Mode = AddrModeFlat;                                                                        \
     } while (0)
 #endif
@@ -80,11 +70,13 @@
 #define STR(...) _str_raw(__VA_ARGS__)
 #define STR_CRLF(...) STR(__VA_ARGS__ \r\n)
 
-#define winerror_CHECK(CLOSURE, MSG)                                                                                   \
+#define winerror_CHECK(CLOSURE, CALL)                                                                                  \
     do                                                                                                                 \
     {                                                                                                                  \
-        if (!((BOOL)(CLOSURE)))                                                                                        \
-            fnErrorExit(STR_CRLF(calling MSG, _fn_metadata));                                                          \
+        CLOSURE;                                                                                                       \
+        DWORD64 dwLastError = GetLastError();                                                                          \
+        if (dwLastError < 0)                                                                                           \
+            fnErrorExit(STR_CRLF(at call CALL, _fn_metadata), dwLastError);                                            \
     } while (0)
 
 #ifndef MAX_PROC_NAME
@@ -99,18 +91,41 @@
 #define MAX_IND_SIZE 4096
 #endif
 
+#ifndef MAX_MDATA_SIZE
+#define MAX_MDATA_SIZE 512
+#endif
+
 #define NO_FLAGS 0
+#define MSG_FLAGS FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
 #define NO_THREADS 0
+
+#ifdef Stackls_32BIT
+#define dbgfn_StackWalk StackWalk
+#define dbgfn_SymFunctionTableAccess SymFunctionTableAccess
+#define dbgfn_SymGetModuleBase SymGetModuleBase
+#define dbgfn_SymFromAddr SymGetSymFromAddr
+#define dbgfn_SymbolInfoPtr PIMAGEHLP_SYMBOL
+#define dbgfn_SymbolInfoObj IMAGEHLP_SYMBOL
+#define dbgfn_SymbolLenIdent MaxNameLength
+#else
+#define dbgfn_StackWalk StackWalk64
+#define dbgfn_SymFunctionTableAccess SymFunctionTableAccess64
+#define dbgfn_SymGetModuleBase SymGetModuleBase64
+#define dbgfn_SymFromAddr SymFromAddr
+#define dbgfn_SymbolInfoPtr PSYMBOL_INFO
+#define dbgfn_SymbolInfoObj SYMBOL_INFO
+#define dbgfn_SymbolLenIdent MaxNameLen
+#endif
 
 #define CTX_MainContextObj pStraceCtx
 #define CTX_ProcessNameStr pStraceCtx->pszProcessName
 #define CTX_LastSymNameStr pStraceCtx->pszLastSymName
-#define CTX_IndicatorStr pStraceCtx->pszStaclIndicator
+#define CTX_IndicatorStr pStraceCtx->pszStackIndicator
 #define CTX_OutputPathStr pStraceCtx->pszOutPath
 #define CTX_OutputFileHandle pStraceCtx->hOutput
 #define CTX_EntryProcessName pStraceCtx->pszEntryProcesssName
 #define CTX_ProcessHandle pStraceCtx->hProcess
-#define CTX_Hep32SnapShot pStraceCtx->hSnapshot
+#define CTX_Help32SnapShot pStraceCtx->hSnapshot
 #define CTX_ProcessEntryBuff pStraceCtx->pProcessEntry
 #define CTX_CurrentStackFrame pStraceCtx->pStackFrame
 #define CTX_MachineContext pStraceCtx->pMachineContext
@@ -121,34 +136,30 @@
 
 typedef struct
 {
-    PTCHAR pszProcessName, pszEntryProcesssName.pszLastSymName, pszStackIndicator, pszOutPath;
+    PTCHAR pszProcessName, pszEntryProcesssName, pszLastSymName, pszStackIndicator, pszOutPath;
     HANDLE hProcess, hSnapshot, hOutput;
     DWORD64 qwDisplacement, qwStackCounter;
-    PROCESS32ENTRY *pProcessEntry;
+    PROCESSENTRY32 *pProcessEntry;
     LPSTACKFRAME pStackFrame;
     CONTEXT *pMachineContext;
-    PSYMBOL_INFO pCurrentSymbol;
+    dbgfn_SymbolInfoPtr pCurrentSymbol;
     BOOL bTraceEnded;
 } STACKLSCTX, *PSTACKLSCTX;
 
-_noreturn_inline void fnErrorExit(LPTSTR pszFunction)
+_noreturn_inline void fnErrorExit(PTCHAR pszMetadata, DWORD64 qwLastError)
 {
-    PVOID pMsgBuf;
-    PVOID pDisplayBuf;
-    DWORD dw = GetLastError();
+    SIZE_T sizeMetadata, sizeMessage;
+    PTCHAR pszMessage;
+    HANDLE hStdOut;
 
-    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, dw,
-                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&pMsgBuf, 0, NULL);
+    sizeMessage =
+        FormatMessage(MSG_FLAGS, NULL, qwLastError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), pszMessage, 0, NULL);
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    StringCbLength(pszMetadata, MAX_MDATA_SIZE, &sizeMetadata);
 
-    pDisplayBuf = (PVOID)LocalAlloc(LMEM_ZEROINIT,
-                                    (lstrlen((PCTSTR)pMsgBuf) + lstrlen((PCTSTR)pszFunction) + 40) * sizeof(TCHAR));
-    StringCchPrintf((LPTSTR)pDisplayBuf, LocalSize(pDisplayBuf) / sizeof(TCHAR), TEXT("%s failed with error %d: %s"),
-                    pszFunction, dw, pMsgBuf);
-    MessageBox(NULL, (PCTSTR)pDisplayBuf, TEXT("Error"), MB_OK);
-
-    LocalFree(pMsgBuf);
-    LocalFree(pDisplayBuf);
-    ExitProcess(dw);
+    WriteFile(hStdOut, pszMessage, sizeMessage, NULL, NULL);
+    WriteFile(hStdOut, pszMetadata, sizeMetadata, NULL, NULL);
+    ExitProcess(qwLastError);
 }
 
 _static_func void fnStacklsAllocateStaticBuffers(PSTACKLSCTX pStraceCtx)
@@ -157,7 +168,7 @@ _static_func void fnStacklsAllocateStaticBuffers(PSTACKLSCTX pStraceCtx)
     _static_obj PROCESSENTRY32 objProcessEntry = {0};
     _static_obj STACKFRAME64 objStackFrame = {0};
     _static_obj CONTEXT objMachineContext = {0};
-    _static_obj SYMBOL_INFO objSymbolInfo = {0};
+    _static_obj dbgfn_SymbolInfoObj objSymbolInfo = {0};
     _static_obj TCHAR aProcessName[MAX_PROC_NAME] = {0};
     _static_obj TCHAR aLastSymName[MAX_SYM_NAME] = {0};
     _static_obj TCHAR aStackIndicator[MAX_IND_SIZE] = {0};
@@ -192,7 +203,7 @@ _coldbed_inline void fnStacklsOpenOutputFile(PSTACKLSCTX pStraceCtx)
 
 _coldbed_inline void fnStacklsOpenSnapshotHandle(PSTACKLSCTX pStraceCtx)
 {
-    winerror_CHECK(CTX_Hep32SnapShot = CreateToolhep32Snapshot(T32Cs_SNAPPROCESS, 0), CreateToolhep32Snapshot);
+    winerror_CHECK(CTX_Help32SnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0), CreateToolhelp32Snapshot);
 }
 
 _coldbed_inline void fnStacklsOpenProcessHandle(PSTACKLSCTX pStraceCtx)
@@ -203,7 +214,7 @@ _coldbed_inline void fnStacklsOpenProcessHandle(PSTACKLSCTX pStraceCtx)
 
 _coldbed_inline void fnStacklsCloseAllHandles(PSTACKLSCTX pStraceCtx)
 {
-    winerror_CHECK(CloseHandle(CTX_StandardOutput), CloseHandle);
+    winerror_CHECK(CloseHandle(CTX_OutputFileHandle), CloseHandle);
     winerror_CHECK(CloseHandle(CTX_Help32SnapShot), CloseHandle);
     winerror_CHECK(CloseHandle(CTX_ProcessHandle), CloseHandle);
 }
@@ -212,12 +223,12 @@ _hotbed_inline void fnStacklsFindProcessHandle(PSTACKLSCTX pStraceCtx)
 {
     int dwStrCmpRes;
     CTX_ProcessEntryBuff->dwSize = sizeof(PROCESSENTRY32);
-    if (Process32First(CTX_Hep32SnapShot, CTX_ProcessEntryBuff))
+    if (Process32First(CTX_Help32SnapShot, CTX_ProcessEntryBuff))
     {
-        while (Process32Next(CTX_Hep32SnapShot, CTX_ProcessEntryBuff))
+        while (Process32Next(CTX_Help32SnapShot, CTX_ProcessEntryBuff))
         {
-            CTX_EntryProcessName = &CTX_ProcessEntryBuff->szExecFile[0];
-            dwStrCmpRes = lstrcmpiW(CTX_ProcessNameStr, CTX_EntryProcessName);
+            CTX_EntryProcessName = &CTX_ProcessEntryBuff->szExeFile[0];
+            dwStrCmpRes = lstrcmpi(CTX_ProcessNameStr, CTX_EntryProcessName);
             if (!dwStrCmpRes)
             {
                 fnStacklsOpenProcessHandle(pStraceCtx);
@@ -230,8 +241,8 @@ _normal_inline void fnStacklsInitiateContextAndSymbols(PSTACKLSCTX pStraceCtx)
 {
     winerror_CHECK(RtlCaptureContext(CTX_MachineContext), RtlCaptureContext);
     winerror_CHECK(SymInitialize(CTX_ProcessHandle, NULL, TRUE), SymInitialize);
-    CTX_CurrentSymbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-    CTX_CurrentSymbol->MaxNameLength = MAX_SYM_NAME;
+    CTX_CurrentSymbol->SizeOfStruct = sizeof(dbgfn_SymbolInfoObj);
+    CTX_CurrentSymbol->dbgfn_SymbolLenIdent = MAX_SYM_NAME;
 }
 
 _normal_inline void fnStacklsInitializeStackWalk(PSTACKLSCTX pStraceCtx)
@@ -247,31 +258,32 @@ _normal_inline void fnStacklsResetIndicatorStr(PSTACKLSCTX pStraceCtx)
 
 _hotbed_inline void fnStacklsLoadTheNextFrame(PSTACKLSCTX pStraceCtx)
 {
-    CTX_StackIsAtBottom = !(StackWalk64(MACHINE_TYPE, *CTX_ProcessHandle, NO_THREADS, CTX_CurrentStackFrame,
-                                        CTX_MachineContext, NULL, SymFunctionTableAccess64, SymGetModuleBase64, NULL));
+    CTX_StackIsAtBottom =
+        !(dbgfn_StackWalk(MACHINE_TYPE, CTX_ProcessHandle, NO_THREADS, CTX_CurrentStackFrame, CTX_MachineContext, NULL,
+                          dbgfn_SymFunctionTableAccess, dbgfn_SymGetModuleBase, NULL));
 }
 
 _hotbed_inline void fnStacklsExtractSymbolFromFrame(PSTACKLSCTX pStraceCtx)
 {
+    winerror_CHECK(dbgfn_SymFromAddr(CTX_ProcessHandle, CTX_CurrentStackFrame->AddrPC.Offset, &CTX_Displacement,
+                                     CTX_CurrentSymbol),
+                   SymFromAddr);
     winerror_CHECK(
-        SymFromAddr(*CTX_ProcessHandle, CTX_CurrentStackFrame->AddrPC.Offset, CTX_Displacement, CTX_CurrentSymbol),
-        SymFromAddr);
-    winerror_CHECK(UnDecorateSymbolName(CTX_CurrentSymbol->Name, (PSTR)CTX_LastSymblStr, MAX_SYM_NAME,
-                                        &CTX_Displacement, UNDNAME_COMPLETE),
-                   UnDecorateSymbolName);
+        UnDecorateSymbolName(CTX_CurrentSymbol->Name, (PSTR)CTX_LastSymNameStr, MAX_SYM_NAME, UNDNAME_COMPLETE),
+        UnDecorateSymbolName);
 }
 
 _hotbed_inline void fnStacklsWriteSymbolToHandle(PSTACKLSCTX pStraceCtx)
 {
     if (CTX_StackIsAtBottom)
         return;
-    DWORD64 qwWritten, qwRead;
+    SIZE_T qwWritten, qwRead;
     fnStacklsResetIndicatorStr(pStraceCtx);
     winerror_CHECK(
-        StringCchPrintf(CTX_IndicatorStr, MAX_IND_SIZE, TEXT("[%lu] %s\n"), CTX_StackCounter, CTX_LastSymStr),
+        StringCchPrintf(CTX_IndicatorStr, MAX_IND_SIZE, TEXT("[%lu] %s\n"), CTX_StackCounter, CTX_LastSymNameStr),
         StringCchPrintf);
     winerror_CHECK(StringCbLength(CTX_IndicatorStr, MAX_IND_SIZE, &qwRead), StringCbLength);
-    winerror_CHECK(WriteFile(CTX_StandardOutput, CTX_IndicatorStr, qwRead, &qwWritten, NULL), WriteFile);
+    winerror_CHECK(WriteFile(CTX_OutputFileHandle, CTX_IndicatorStr, qwRead, (LPDWORD)&qwWritten, NULL), WriteFile);
 }
 
 _hotbed_inline void fnStacklsIterateAndWalkStack(PSTACKLSCTX pStraceCtx)
@@ -303,23 +315,23 @@ void fnParseCmdlineArgs(int nArgs, PTCHAR *szArglist, PSTACKLSCTX pStraceCtx)
         fnDisplayHelp();
     else if (nArgs == 2)
     {
-        if (!lstrcmpiW(szArglist[1], "--help") || !lstrcmpiW(szArglist[1], "-h"))
+        if (!lstrcmpi(szArglist[1], "--help") || !lstrcmpi(szArglist[1], "-h"))
             fnDisplayHelp();
         else
         {
-            DWORD64 qwArglen;
-            winerror_CHECK(StringCbLength(&szArglist[1][0], MAX_PROC_SIZE, &qwArglen), StringCbLength);
-            winerror_CHECK(StringCbCopy(CTX_ProcessNameStr, &szArglist[1][0], qwArglen));
+            SIZE_T qwArglen;
+            winerror_CHECK(StringCbLength(&szArglist[1][0], MAX_PROC_NAME, &qwArglen), StringCbLength);
+            winerror_CHECK(StringCbCopy(CTX_ProcessNameStr, qwArglen, szArglist[1]), StringCbCopy);
             CTX_OutputPathStr = NULL;
         }
     }
     else if (nArgs == 4)
     {
-        DWORD qwProcessNameLen, qwOutputPathLen;
+        SIZE_T qwProcessNameLen, qwOutputPathLen;
         winerror_CHECK(StringCbLength(&szArglist[2][0], MAX_PATH, &qwOutputPathLen), StrinCbLength);
-        winerror_CHECK(StringCbLength(&szArglist[3][0], MAX_PROC_SIZE, &qwProcessNameLen), StrinCbLength);
-        winerror_CHECK(StringCbCopy(CTX_OutputPathStr, &szArglist[2][0], qwOutptuPathLen), StringCbCopy);
-        winerror_CHECK(StringCbCopy(CTX_ProcessNameStr, &szArglist[3][0], qwProcessNameLen), StringCbCopy);
+        winerror_CHECK(StringCbLength(&szArglist[3][0], MAX_PROC_NAME, &qwProcessNameLen), StrinCbLength);
+        winerror_CHECK(StringCbCopy(CTX_OutputPathStr, qwOutputPathLen, szArglist[2]), StringCbCopy);
+        winerror_CHECK(StringCbCopy(CTX_ProcessNameStr, qwProcessNameLen, szArglist[3]), StringCbCopy);
     }
     else
     {
@@ -327,13 +339,13 @@ void fnParseCmdlineArgs(int nArgs, PTCHAR *szArglist, PSTACKLSCTX pStraceCtx)
     }
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPCTSTR lpCmdLine, int nCmdShow)
 {
     int nArgs;
     PTCHAR *szArglist;
     PSTACKLSCTX pStraceCtx;
 
-    winerror_CHECK(szArglist = CommandLineToArgvW(lpCmdLine, &nArgs), CommandLineToArgvW);
+    winerror_CHECK(szArglist = (PTCHAR *)CommandLineToArgvW((LPWSTR)lpCmdLine, &nArgs), CommandLineToArgvW);
 
     fnStacklsAllocateStaticBuffers(pStraceCtx);
     fnParseCmdlineArgs(nArgs, szArglist, pStraceCtx);
