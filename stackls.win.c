9a9be368eb95b536978bb18f06caa0ffcb0075bb
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-24 23:13:02 +0330

    moved declrations

--- stackls.win.c
+++ stackls.win.c
@@ -1,152 +1,16 @@
 #define WIN32_LEAN_AND_MEAN
-#include <dbghelp.h>
-#include <errhandlingapi.h>
+#include <windows.h>
 #include <shellapi.h>
 #include <strsafe.h>
 #include <tlhelp32.h>
 #include <winbase.h>
-#include <windows.h>
+#include <dbghelp.h>
+#include <errhandlingapi.h>
 
-#ifdef __GNUC__
-#define _normal_inline static inline __attribute__((always_inline))
-#define _hotbed_inline static inline __attribute__((always_inline, hot))
-#define _coldbed_inline static inline __attribute__((always_inline, cold))
-#define _noreturn_inline static inline __attribute__((noreturn, always_inline, hot))
-#define _fn_metadata file __FILE__, line __LINE__
-#define _fn_name __PRETTY_FUNCTION__
-#elif _MSC_VER
-#define _normal_inline static inline __declspec(always_inline)
-#define _hotbed_inline static inline __declspec(always_inline, hot)
-#define _coldbed_inline static inline __declspec(always_inline, cold)
-#define _noreturn_inline static inline __declspec(noreturn, always_inline, hot)
-#define _fn_metadata file __FILE__, line __LINE__
-#define _fn_name __func__
-#endif
+#include "stackls.com.h"
+#include "stackls.win.h"
 
-#define _static_func static
-#define _static_obj static
-
-#if defined(_M_IX86) || defined(__x86__)
-#define MACHINE_TYPE IMAGE_FILE_MACHINE_I386
-#define ASSIGN_MACHINE_VALUES(STACKW, CONTEXT)                                                                         \
-    do                                                                                                                 \
-    {                                                                                                                  \
-        STACKW->AddrPC.Offset = CONTEXT->Eip;                                                                          \
-        STACKW->AddrPC.Mode = AddrModeFlat;                                                                            \
-        STACKW->AddrFrame.Offset = CONTEXT->Ebp;                                                                       \
-        STACKW->AddrFrame.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrStack.Offset = CONTEXT->Esp;                                                                       \
-        STACKW->AddrStack.Mode = AddrModeFlat;                                                                         \
-    } while (0)
-#elif defined(_M_IX64) || defined(_M_AMD64) || defined(__x86_64__) || defined(__amd64__)
-#define MACHINE_TYPE IMAGE_FILE_MACHINE_AMD64
-#define ASSIGN_MACHINE_VALUES(STACKW, CONTEXT)                                                                         \
-    do                                                                                                                 \
-    {                                                                                                                  \
-        STACKW->AddrPC.Offset = CONTEXT->Rip;                                                                          \
-        STACKW->AddrPC.Mode = AddrModeFlat;                                                                            \
-        STACKW->AddrFrame.Offset = CONTEXT->Rbp;                                                                       \
-        STACKW->AddrFrame.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrStack.Offset = CONTEXT->Rsp;                                                                       \
-        STACKW->AddrStack.Mode = AddrModeFlat;                                                                         \
-    } while (0)
-#elif defined(_M_IA64) || defined(__ia64__)
-#define MACHINE_TYPE IMAGE_FILE_MACHINE_IA64
-#define ASSIGN_MACHINE_VALUES(STACKW, CONTEXT)                                                                         \
-    do                                                                                                                 \
-    {                                                                                                                  \
-        STACKW->AddrPC.Offset = CONTEXT->StIIP;                                                                        \
-        STACKW->AddrPC.Mode = AddrModeFlat;                                                                            \
-        STACKW->AddrFrame.Offset = CONTEXT->IntSp;                                                                     \
-        STACKW->AddrFrame.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrStack.Offset = CONTEXT->IntSP;                                                                     \
-        STACKW->AddrStack.Mode = AddrModeFlat;                                                                         \
-        STACKW->AddrBStore.Offset = CONTEXT->RsBSP;                                                                    \
-        STACKW->AddrBStore.Mode = AddrModeFlat;                                                                        \
-    } while (0)
-#endif
-
-#define _str_raw(...) #__VA_ARGS__
-#define STR(...) _str_raw(__VA_ARGS__)
-#define STR_CRLF(...) STR(__VA_ARGS__ \r\n)
-
-#define winerror_CHECK(CLOSURE, CALL)                                                                                  \
-    do                                                                                                                 \
-    {                                                                                                                  \
-        CLOSURE;                                                                                                       \
-        DWORD64 dwLastError = GetLastError();                                                                          \
-        if (dwLastError < 0)                                                                                           \
-            fnErrorExit(STR_CRLF(at call CALL, _fn_metadata), dwLastError);                                            \
-    } while (0)
-
-#ifndef MAX_PROC_NAME
-#define MAX_PROC_NAME 128
-#endif
-
-#ifndef MAX_SYM_NAME
-#define MAX_SYM_NAME ((2048 * sizeof(TCHAR)) + sizeof(SYMBOL_INFO))
-#endif
-
-#ifndef MAX_IND_SIZE
-#define MAX_IND_SIZE 4096
-#endif
-
-#ifndef MAX_MDATA_SIZE
-#define MAX_MDATA_SIZE 512
-#endif
-
-#define NO_FLAGS 0
-#define MSG_FLAGS FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
-#define NO_THREADS 0
-
-#ifdef Stackls_32BIT
-#define dbgfn_StackWalk StackWalk
-#define dbgfn_SymFunctionTableAccess SymFunctionTableAccess
-#define dbgfn_SymGetModuleBase SymGetModuleBase
-#define dbgfn_SymFromAddr SymGetSymFromAddr
-#define dbgfn_SymbolInfoPtr PIMAGEHLP_SYMBOL
-#define dbgfn_SymbolInfoObj IMAGEHLP_SYMBOL
-#define dbgfn_SymbolLenIdent MaxNameLength
-#else
-#define dbgfn_StackWalk StackWalk64
-#define dbgfn_SymFunctionTableAccess SymFunctionTableAccess64
-#define dbgfn_SymGetModuleBase SymGetModuleBase64
-#define dbgfn_SymFromAddr SymFromAddr
-#define dbgfn_SymbolInfoPtr PSYMBOL_INFO
-#define dbgfn_SymbolInfoObj SYMBOL_INFO
-#define dbgfn_SymbolLenIdent MaxNameLen
-#endif
-
-#define CTX_MainContextObj pStraceCtx
-#define CTX_ProcessNameStr pStraceCtx->pszProcessName
-#define CTX_LastSymNameStr pStraceCtx->pszLastSymName
-#define CTX_IndicatorStr pStraceCtx->pszStackIndicator
-#define CTX_OutputPathStr pStraceCtx->pszOutPath
-#define CTX_OutputFileHandle pStraceCtx->hOutput
-#define CTX_EntryProcessName pStraceCtx->pszEntryProcesssName
-#define CTX_ProcessHandle pStraceCtx->hProcess
-#define CTX_Help32SnapShot pStraceCtx->hSnapshot
-#define CTX_ProcessEntryBuff pStraceCtx->pProcessEntry
-#define CTX_CurrentStackFrame pStraceCtx->pStackFrame
-#define CTX_MachineContext pStraceCtx->pMachineContext
-#define CTX_StackIsAtBottom pStraceCtx->bTraceEnded
-#define CTX_CurrentSymbol pStraceCtx->pCurrentSymbol
-#define CTX_Displacement pStraceCtx->qwDisplacement
-#define CTX_StackCounter pStraceCtx->qwStackCounter
-
-typedef struct
-{
-    PTCHAR pszProcessName, pszEntryProcesssName, pszLastSymName, pszStackIndicator, pszOutPath;
-    HANDLE hProcess, hSnapshot, hOutput;
-    DWORD64 qwDisplacement, qwStackCounter;
-    PROCESSENTRY32 *pProcessEntry;
-    LPSTACKFRAME pStackFrame;
-    CONTEXT *pMachineContext;
-    dbgfn_SymbolInfoPtr pCurrentSymbol;
-    BOOL bTraceEnded;
-} STACKLSCTX, *PSTACKLSCTX;
-
-_noreturn_inline void fnErrorExit(PTCHAR pszMetadata, DWORD64 qwLastError)
+void fnErrorExit(PTCHAR pszMetadata, DWORD64 qwLastError)
 {
     SIZE_T sizeMetadata, sizeMessage;
     PTCHAR pszMessage;
@@ -162,7 +26,7 @@ _noreturn_inline void fnErrorExit(PTCHAR pszMetadata, DWORD64 qwLastError)
     ExitProcess(qwLastError);
 }
 
-_static_func void fnStacklsAllocateStaticBuffers(PSTACKLSCTX pStraceCtx)
+void fnStacklsAllocateStaticBuffers(PSTACKLSCTX pStraceCtx)
 {
     _static_obj STACKLSCTX objStacklsMain = {0};
     _static_obj PROCESSENTRY32 objProcessEntry = {0};
@@ -187,7 +51,7 @@ _static_func void fnStacklsAllocateStaticBuffers(PSTACKLSCTX pStraceCtx)
     CTX_OutputPathStr = &aOutputPath[0];
 }
 
-_coldbed_inline void fnStacklsOpenOutputFile(PSTACKLSCTX pStraceCtx)
+void fnStacklsOpenOutputFile(PSTACKLSCTX pStraceCtx)
 {
     if (!CTX_OutputPathStr)
     {
@@ -201,25 +65,25 @@ _coldbed_inline void fnStacklsOpenOutputFile(PSTACKLSCTX pStraceCtx)
     }
 }
 
-_coldbed_inline void fnStacklsOpenSnapshotHandle(PSTACKLSCTX pStraceCtx)
+void fnStacklsOpenSnapshotHandle(PSTACKLSCTX pStraceCtx)
 {
     winerror_CHECK(CTX_Help32SnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0), CreateToolhelp32Snapshot);
 }
 
-_coldbed_inline void fnStacklsOpenProcessHandle(PSTACKLSCTX pStraceCtx)
+void fnStacklsOpenProcessHandle(PSTACKLSCTX pStraceCtx)
 {
     winerror_CHECK(CTX_ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, CTX_ProcessEntryBuff->th32ProcessID),
                    OpenProcess);
 }
 
-_coldbed_inline void fnStacklsCloseAllHandles(PSTACKLSCTX pStraceCtx)
+void fnStacklsCloseAllHandles(PSTACKLSCTX pStraceCtx)
 {
     winerror_CHECK(CloseHandle(CTX_OutputFileHandle), CloseHandle);
     winerror_CHECK(CloseHandle(CTX_Help32SnapShot), CloseHandle);
     winerror_CHECK(CloseHandle(CTX_ProcessHandle), CloseHandle);
 }
 
-_hotbed_inline void fnStacklsFindProcessHandle(PSTACKLSCTX pStraceCtx)
+void fnStacklsFindProcessHandle(PSTACKLSCTX pStraceCtx)
 {
     int dwStrCmpRes;
     CTX_ProcessEntryBuff->dwSize = sizeof(PROCESSENTRY32);
@@ -237,7 +101,7 @@ _hotbed_inline void fnStacklsFindProcessHandle(PSTACKLSCTX pStraceCtx)
     }
 }
 
-_normal_inline void fnStacklsInitiateContextAndSymbols(PSTACKLSCTX pStraceCtx)
+void fnStacklsInitiateContextAndSymbols(PSTACKLSCTX pStraceCtx)
 {
     winerror_CHECK(RtlCaptureContext(CTX_MachineContext), RtlCaptureContext);
     winerror_CHECK(SymInitialize(CTX_ProcessHandle, NULL, TRUE), SymInitialize);
@@ -245,25 +109,25 @@ _normal_inline void fnStacklsInitiateContextAndSymbols(PSTACKLSCTX pStraceCtx)
     CTX_CurrentSymbol->dbgfn_SymbolLenIdent = MAX_SYM_NAME;
 }
 
-_normal_inline void fnStacklsInitializeStackWalk(PSTACKLSCTX pStraceCtx)
+void fnStacklsInitializeStackWalk(PSTACKLSCTX pStraceCtx)
 {
     SecureZeroMemory(CTX_CurrentStackFrame, sizeof(STACKFRAME64));
     ASSIGN_MACHINE_VALUES(CTX_CurrentStackFrame, CTX_MachineContext);
 }
 
-_normal_inline void fnStacklsResetIndicatorStr(PSTACKLSCTX pStraceCtx)
+void fnStacklsResetIndicatorStr(PSTACKLSCTX pStraceCtx)
 {
     SecureZeroMemory(CTX_IndicatorStr, MAX_IND_SIZE * sizeof(TCHAR));
 }
 
-_hotbed_inline void fnStacklsLoadTheNextFrame(PSTACKLSCTX pStraceCtx)
+void fnStacklsLoadTheNextFrame(PSTACKLSCTX pStraceCtx)
 {
     CTX_StackIsAtBottom =
         !(dbgfn_StackWalk(MACHINE_TYPE, CTX_ProcessHandle, NO_THREADS, CTX_CurrentStackFrame, CTX_MachineContext, NULL,
                           dbgfn_SymFunctionTableAccess, dbgfn_SymGetModuleBase, NULL));
 }
 
-_hotbed_inline void fnStacklsExtractSymbolFromFrame(PSTACKLSCTX pStraceCtx)
+void fnStacklsExtractSymbolFromFrame(PSTACKLSCTX pStraceCtx)
 {
     winerror_CHECK(dbgfn_SymFromAddr(CTX_ProcessHandle, CTX_CurrentStackFrame->AddrPC.Offset, &CTX_Displacement,
                                      CTX_CurrentSymbol),
@@ -273,7 +137,7 @@ _hotbed_inline void fnStacklsExtractSymbolFromFrame(PSTACKLSCTX pStraceCtx)
         UnDecorateSymbolName);
 }
 
-_hotbed_inline void fnStacklsWriteSymbolToHandle(PSTACKLSCTX pStraceCtx)
+void fnStacklsWriteSymbolToHandle(PSTACKLSCTX pStraceCtx)
 {
     if (CTX_StackIsAtBottom)
         return;
@@ -286,7 +150,7 @@ _hotbed_inline void fnStacklsWriteSymbolToHandle(PSTACKLSCTX pStraceCtx)
     winerror_CHECK(WriteFile(CTX_OutputFileHandle, CTX_IndicatorStr, qwRead, (LPDWORD)&qwWritten, NULL), WriteFile);
 }
 
-_hotbed_inline void fnStacklsIterateAndWalkStack(PSTACKLSCTX pStraceCtx)
+void fnStacklsIterateAndWalkStack(PSTACKLSCTX pStraceCtx)
 {
     fnStacklsOpenOutputFile(pStraceCtx);
     fnStacklsOpenSnapshotHandle(pStraceCtx);
@@ -350,4 +214,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPCTSTR lpCmdLi
     fnStacklsAllocateStaticBuffers(pStraceCtx);
     fnParseCmdlineArgs(nArgs, szArglist, pStraceCtx);
     fnStacklsIterateAndWalkStack(pStraceCtx);
-}
+}  
